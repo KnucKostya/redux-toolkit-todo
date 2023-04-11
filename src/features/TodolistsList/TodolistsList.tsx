e4c8ac953a636f1dbf23e83c8ef5f6c33d278b87
My Name <knuckostya@gmail.com>
2023-04-11 21:43:53 +0300

    RTK - with extra Reducers

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -9,7 +9,7 @@ import {
     removeTodolistTC,
     TodolistDomainType, todolistsActions
 } from './todolists-reducer'
-import { addTaskTC, removeTaskTC, TasksStateType, updateTaskTC } from './tasks-reducer'
+import {removeTaskTC, TasksStateType, tasksThunks, updateTaskTC} from './tasks-reducer'
 import { TaskStatuses } from 'api/todolists-api'
 import { Grid, Paper } from '@mui/material'
 import { AddItemForm } from 'components/AddItemForm/AddItemForm'
@@ -42,7 +42,7 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
     }, [])
 
     const addTask = useCallback(function (title: string, todolistId: string) {
-        const thunk = addTaskTC(title, todolistId)
+        const thunk = tasksThunks.addTask({title, todolistId})
         dispatch(thunk)
     }, [])
 
